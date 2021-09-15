4c8dfabd87aed2da8a18d2aeeb7d6ea9d7ffc0be
Joabesv <joabevarjao123@gmail.com>
2021-09-15 13:00:35 -0300

    middleware to verify account cpf added

--- src/index.js
+++ src/index.js
@@ -15,6 +15,21 @@ const customers = [];
  * statement - []
  */
 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(404).json({ error: 'Customer not found' });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body;
 
@@ -36,16 +51,9 @@ app.post('/account', (req, res) => {
   return res.status(201).send();
 });
 
-app.get('/statement', (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(404).json({ error: 'Customer not found' });
-  }
-
-  return res.json(customer.statement);
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  res.json(customer.statement);
 });
 
 app.listen(port, () => {
